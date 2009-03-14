b15adc5e5ebe6d902b503bb43ace4afc997da8b8
Matt Ball <ball.matt@gmail.com>
2009-03-13 17:58:16 -0700

    Initial commit

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MBCoverFlowView
+//
+//  Created by Matt Ball on 3/13/09.
+//  Copyright Daybreak Apps 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
